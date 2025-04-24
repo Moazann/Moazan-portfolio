b61042884f70f379a07279209588e63cf720da9d
moazan-ad <moazan@agiledistrict.com>
2025-04-24 21:20:39 +0500

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Moazan-portfolio/",
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
 });
